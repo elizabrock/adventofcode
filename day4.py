96c351dad3c832d1d5df4305aa67bbbe3e4d398f
Eliza Brock Marcum <eliza@elizabrocksoftware.com>
2016-01-16 21:35:52 -0600

    Day 4, Part 2

--- day4.py
+++ day4.py
@@ -2,15 +2,17 @@ import hashlib
 
 class Day4:
     @classmethod
-    def find_n_for_key(self, key):
+    def find_n_for_key(self, key, trailing_zeros):
         i = -1
         result = ''
-        while not result.startswith('00000'):
+        while not result.startswith('0'*trailing_zeros):
             i += 1
             value = "{0}{1}".format(key, i)
             result = hashlib.md5(bytes(value, 'utf-8')).hexdigest()
         return i
 
 if __name__ == "__main__":
-    result = Day4.find_n_for_key('yzbqklnj')
-    print("The hash salt is {0}".format(result))
+    result = Day4.find_n_for_key('yzbqklnj', 5)
+    print("The hash salt for part 1 is {0}".format(result))
+    result = Day4.find_n_for_key('yzbqklnj', 6)
+    print("The hash salt for part 2 is {0}".format(result))
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -5,11 +5,11 @@ class TestDay4(TestCase):
     def test_find_n_for_key_abcdef(self):
         input = 'abcdef'
         expected = 609043
-        actual = Day4.find_n_for_key(input)
+        actual = Day4.find_n_for_key(input, 5)
         self.assertEqual(expected, actual)
 
     def test_find_n_for_key_pqrstuv(self):
         input = 'pqrstuv'
         expected = 1048970
-        actual = Day4.find_n_for_key(input)
+        actual = Day4.find_n_for_key(input, 5)
         self.assertEqual(expected, actual)
